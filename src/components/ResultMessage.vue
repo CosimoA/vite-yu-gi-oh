83c61b6f99495fa4e1b3e74f7518c9e7fc944936
CosimoA <cosimo.archip@gmail.com>
2024-01-11 17:15:44 +0100

    BONUS: Create Result Count compoinent

---
+++ src/components/ResultMessage.vue
@@ -0,0 +1,41 @@
+<script>
+import { store } from '../store';
+export default {
+    name: 'ResultMessage',
+    data() {
+        return {
+            store,
+        }
+    },
+
+    computed: {
+        resultData() {
+            return store.cardsList.length;
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div>
+        <span>Trovato <strong>{{ resultData }}</strong> Risultati</span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins.scss' as *;
+
+div {
+    background-color: rgb(31, 31, 31);
+    margin: 0 1rem -0.8rem 1rem;
+    min-width: 230px;
+
+    span {
+        color: whitesmoke;
+        padding: 1rem 0;
+        display: block;
+        text-align: center;
+    }
+}
+</style>
